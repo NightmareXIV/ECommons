9dbf6bbe35adc775272ef22f689bb47dfb9bb85a
Eternita-S <eternitast@gmail.com>
2022-05-09 22:19:19 +0300

    code

--- ECommons/ImGuiMethods/ImGuiEx.cs
+++ ECommons/ImGuiMethods/ImGuiEx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Dalamud.Interface;
+using Dalamud.Interface.Internal.Notifications;
+using ECommons.DalamudServices;
 using ImGuiNET;
 
 namespace ECommons.ImGuiMethods
@@ -24,6 +28,42 @@ namespace ECommons.ImGuiMethods
             var result = ImGui.Button($"{icon.ToIconString()}##{icon.ToIconString()}-{id}");
             ImGui.PopFont();
             return result;
+        }
+
+        public static float Measure(Action func)
+        {
+            var pos = ImGui.GetCursorPosX();
+            func();
+            ImGui.SameLine(0, 0);
+            var diff = ImGui.GetCursorPosX() - pos;
+            ImGui.Dummy(Vector2.Zero);
+            return diff;
+        }
+
+        public static void InputHex(string name, ref uint hexInt)
+        {
+            var text = $"{hexInt:X}";
+            if (ImGui.InputText(name, ref text, 8))
+            {
+                if (uint.TryParse(text, NumberStyles.HexNumber, null, out var num))
+                {
+                    hexInt = num;
+                }
+            }
+        }
+
+        public static void TextCopy(string text)
+        {
+            ImGui.TextUnformatted(text);
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetMouseCursor(ImGuiMouseCursor.Hand);
+            }
+            if (ImGui.IsItemClicked(ImGuiMouseButton.Left))
+            {
+                ImGui.SetClipboardText(text);
+                Svc.PluginInterface.UiBuilder.AddNotification("Text copied to clipboard", null, NotificationType.Success);
+            }
         }
     }
 }
