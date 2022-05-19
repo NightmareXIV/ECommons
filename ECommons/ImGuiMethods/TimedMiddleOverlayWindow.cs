d69d50c7f8ec40792409419006a9ec60ed4466c1
Eternita-S <eternitast@gmail.com>
2022-05-20 01:06:41 +0300

    update

--- ECommons/ImGuiMethods/TimedMiddleOverlayWindow.cs
+++ ECommons/ImGuiMethods/TimedMiddleOverlayWindow.cs
@@ -11,72 +11,19 @@ using System.Threading.Tasks;
 
 namespace ECommons.ImGuiMethods
 {
-    public class TimedMiddleOverlayWindow : Window, IDisposable
+    public class TimedMiddleOverlayWindow : MiddleOverlayWindow
     {
-        int? TopOffset = null;
-        Vector4? bgCol = null;
-        Vector2 pos = Vector2.Zero;
-        WindowSystem ws = new();
-        Action draw;
         long destroyAt;
-        bool disposed = false;
-        public TimedMiddleOverlayWindow(string name, long destroyAfterMS, Action draw, int? topOffset = null, Vector4? bgCol = null) : base(name, ImGuiWindowFlags.NoInputs
-            | ImGuiWindowFlags.NoNav
-            | ImGuiWindowFlags.NoTitleBar
-            | ImGuiWindowFlags.NoFocusOnAppearing
-            | ImGuiWindowFlags.NoSavedSettings
-            | ImGuiWindowFlags.NoScrollbar
-            | ImGuiWindowFlags.AlwaysAutoResize, true)
+        public TimedMiddleOverlayWindow(string name, long destroyAfterMS, Action draw, int? topOffset = null, Vector4? bgCol = null) : base(name, draw, topOffset, bgCol)
         {
-            TopOffset = topOffset;
-            this.bgCol = bgCol;
             this.destroyAt = Environment.TickCount64 + destroyAfterMS;
-            this.draw = draw;
-            ws.AddWindow(this);
-            this.IsOpen = true;
-            Svc.PluginInterface.UiBuilder.Draw += ws.Draw;
-        }
-
-        public override bool DrawConditions()
-        {
-            return Svc.ClientState.LocalPlayer != null;
-        }
-
-        public override void Draw()
-        {
-            this.draw();
-            pos = ImGui.GetWindowSize();
-        }
-
-        public override void PreDraw()
-        {
-            base.PreDraw();
-            ImGui.SetNextWindowPos(new Vector2(ImGuiHelpers.MainViewport.Size.X / 2 - pos.X / 2,
-                TopOffset ?? ImGuiHelpers.MainViewport.Size.Y / 3));
-            if (bgCol.HasValue) ImGui.PushStyleColor(ImGuiCol.WindowBg, bgCol.Value);
-        }
-
-        public override void PostDraw()
-        {
-            base.PostDraw();
-            if (bgCol.HasValue) ImGui.PopStyleColor();
         }
 
         public override void Update()
         {
-            if(Environment.TickCount64 > destroyAt)
+            if (Environment.TickCount64 > destroyAt)
             {
                 Dispose();
-            }
-        }
-
-        public void Dispose()
-        {
-            if (!disposed)
-            {
-                disposed = true;
-                Svc.PluginInterface.UiBuilder.Draw -= ws.Draw;
-                ws.RemoveWindow(this);
             }
         }
     }
