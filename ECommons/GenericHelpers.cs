45a4d069c17340d8be462c0059413ed14a9e1d4c
Eternita-S <eternitast@gmail.com>
2022-06-17 00:42:12 +0300

    code

--- ECommons/GenericHelpers.cs
+++ ECommons/GenericHelpers.cs
@@ -221,6 +221,20 @@ namespace ECommons
             }
         }
 
+        public static bool TryGetFirst<K>(this IEnumerable<K> enumerable, Func<K, bool> predicate, out K value)
+        {
+            try
+            {
+                value = enumerable.First(predicate);
+                return true;
+            }
+            catch (Exception)
+            {
+                value = default;
+                return false;
+            }
+        }
+
         public static bool TryGetAddonByName<T>(string Addon, out T* AddonPtr) where T : unmanaged
         {
             var a = Svc.GameGui.GetAddonByName(Addon, 1);
