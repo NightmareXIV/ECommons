2c5ec75e35c91bfddbcc6fe25148ce10ac916e98
Limiana <5073202+Limiana@users.noreply.github.com>
2022-10-17 02:18:53 +0300

    fix excel resolver

--- ECommons/ExcelResolver.cs
+++ ECommons/ExcelResolver.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECommons.DalamudServices;
+using Dalamud.Game.ClientState.Resolvers;
 
 #nullable enable
 namespace ECommons
@@ -15,7 +16,7 @@ namespace ECommons
     /// This object resolves a rowID within an Excel sheet.
     /// </summary>
     /// <typeparam name="T">The type of Lumina sheet to resolve.</typeparam>
-    public class ExcelResolver<T> where T : ExcelRow
+    public class ExcelResolver<T> : IEquatable<ExcelResolver<T>> where T : ExcelRow
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ExcelResolver{T}"/> class.
@@ -41,12 +42,42 @@ namespace ECommons
         /// </summary>
         public T? GameData => Svc.Data.GetExcelSheet<T>()?.GetRow(this.Id);
 
+        public override bool Equals(object? obj)
+        {
+            return obj is ExcelResolver<T> resolver &&
+                   Id == resolver.Id;
+        }
+        public bool Equals(ExcelResolver<T>? resolver)
+        {
+            return resolver != null && Id == resolver.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
         /// <summary>
         /// Gets GameData linked to this excel row with the specified language.
         /// </summary>
         /// <param name="language">The language.</param>
         /// <returns>The ExcelRow in the specified language.</returns>
         public T? GetWithLanguage(ClientLanguage language) => Svc.Data.GetExcelSheet<T>(language)?.GetRow(this.Id);
+
+        public static bool operator ==(ExcelResolver<T>? left, ExcelResolver<T>? right)
+        {
+            return EqualityComparer<ExcelResolver<T>>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(ExcelResolver<T>? left, ExcelResolver<T>? right)
+        {
+            return !(left == right);
+        }
+
+        public bool ShouldSerializeGameData()
+        {
+            return false;
+        }
     }
 
     public static class ExcelResolverExtensions
