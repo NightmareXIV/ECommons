e9128d8afc734110ff6f5994a4a38b750a792fc6
Limiana <5073202+Limiana@users.noreply.github.com>
2022-10-17 14:15:41 +0300

    ExcelHelper

---
+++ ECommons/ExcelServices/ExcelHelper.cs
@@ -0,0 +1,33 @@
+﻿using ECommons.DalamudServices;
+using Lumina.Excel.GeneratedSheets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ExcelServices
+{
+    public static class ExcelHelper
+    {
+        public static World GetWorldByName(string name)
+        {
+            if(Svc.Data.GetExcelSheet<World>().TryGetFirst(x => x.Name.ToString().EqualsIgnoreCase(name), out var result))
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public static bool TryGetWorldByName(string name, out World result)
+        {
+            result = GetWorldByName(name);
+            return result != null;
+        }
+
+        public static string[] GetPublicWorlds(Region? region)
+        {
+            return Svc.Data.GetExcelSheet<World>().Where(x => (region == null && x.Region.EqualsAny(Enum.GetValues<Region>().Select(z => (byte)z).ToArray())) || (region.HasValue && x.Region == (byte)region.Value)).Select(x => x.Name.ToString()).ToArray();
+        }
+    }
+}
---
+++ ECommons/ExcelServices/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ExcelServices
+{
+    public enum Region
+    {
+        JP = 1, NA = 2, EU = 3, OC = 4
+    }
+}
