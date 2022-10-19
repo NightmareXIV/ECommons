8d0a8de171023b8ef046713fc63c275cd3f247ae
Limiana <5073202+Limiana@users.noreply.github.com>
2022-10-19 11:35:57 +0300

    action helper

---
+++ ECommons/ExcelServices/ExcelActionHelper.cs
@@ -0,0 +1,19 @@
+﻿using ECommons.DalamudServices;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ExcelServices
+{
+    public unsafe static class ExcelActionHelper
+    {
+        public static float GetActionCooldown(uint id)
+        {
+            var detail = ActionManager.Instance()->GetRecastGroupDetail(Svc.Data.GetExcelSheet<Lumina.Excel.GeneratedSheets.Action>().GetRow(id).CooldownGroup - 1);
+            return detail->IsActive == 1 ? detail->Total - detail->Elapsed : 0;
+        }
+    }
+}
