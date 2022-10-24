173fa2455a196d95aeb074807ed251941db635ab
Limiana <5073202+Limiana@users.noreply.github.com>
2022-10-25 00:24:36 +0300

    Reflection updates

--- ECommons/Reflection/ReflectionHelper.cs
+++ ECommons/Reflection/ReflectionHelper.cs
@@ -10,11 +10,17 @@ namespace ECommons.Reflection
     public static class ReflectionHelper
     {
         const BindingFlags AllFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+        const BindingFlags StaticFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
 
         public static object GetFoP(this object obj, string name)
         {
             return obj.GetType().GetField(name, AllFlags)?.GetValue(obj) 
                 ?? obj.GetType().GetProperty(name, AllFlags)?.GetValue(obj);
+        }
+
+        public static T GetFoP<T>(this object obj, string name)
+        {
+            return (T)GetFoP(obj, name);
         }
 
         public static void SetFoP(this object obj, string name, object value)
@@ -27,6 +33,30 @@ namespace ECommons.Reflection
             else
             {
                 obj.GetType().GetProperty(name, AllFlags).SetValue(obj, value);
+            }
+        }
+
+        public static object GetStaticFoP(this object obj, string type, string name)
+        {
+            return obj.GetType().Assembly.GetType(type).GetField(name, AllFlags)?.GetValue(null)
+                ?? obj.GetType().Assembly.GetType(type).GetProperty(name, AllFlags)?.GetValue(null);
+        }
+
+        public static T GetStaticFoP<T>(this object obj, string type, string name)
+        {
+            return (T)GetStaticFoP(obj, type, name);
+        }
+
+        public static void SetStaticFoP(this object obj, string type, string name, object value)
+        {
+            var field = obj.GetType().Assembly.GetType(type).GetField(name, AllFlags);
+            if (field != null)
+            {
+                field.SetValue(null, value);
+            }
+            else
+            {
+                obj.GetType().Assembly.GetType(type).GetProperty(name, AllFlags).SetValue(null, value);
             }
         }
     }
