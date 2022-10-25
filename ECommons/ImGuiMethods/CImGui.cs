0fd065b22534f7fabcf8d13b4e96cd5dde88bd02
Limiana <5073202+Limiana@users.noreply.github.com>
2022-10-26 01:28:41 +0300

    Imgui stuff

---
+++ ECommons/ImGuiMethods/CImGui.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ImGuiMethods
+{
+    public class CImGui
+    {
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void igBringWindowToDisplayFront(IntPtr ptr);
+
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void igBringWindowToDisplayBack(IntPtr ptr);
+
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr igGetCurrentWindow();
+    }
+}
---
+++ ECommons/ImGuiMethods/GradientColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ImGuiMethods
+{
+    public static class GradientColor
+    {
+        public static Vector4 Get(Vector4 start, Vector4 end, int Miliseconds = 1000)
+        {
+            var delta = (end - start) / (int)Miliseconds;
+            var time = Environment.TickCount % (Miliseconds * 2);
+            if (Environment.TickCount % (Miliseconds * 2) < Miliseconds)
+            {
+                return start + delta * (float)(time / 2);
+            }
+            else
+            {
+                return end - delta * ((float)((time) / 2));
+            }
+        }
+    }
+}
