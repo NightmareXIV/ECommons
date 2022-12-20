713e77d5655839d30ff93a01a0d3c01c3b84f77a
Limiana <5073202+Limiana@users.noreply.github.com>
2022-12-20 03:31:25 +0300

    territories

---
+++ ECommons/ExcelServices/TerritoryEnumeration/ResidentalAreas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ExcelServices.TerritoryEnumeration
+{
+    public static class ResidentalAreas
+    {
+        public const ushort Mist = 339;
+        public const ushort The_Lavender_Beds = 340;
+        public const ushort The_Goblet = 341;
+        public const ushort Shirogane = 641;
+        public const ushort Empyreum = 979;
+    }
+}
