4628b49e65671ee90961b58f4c218f23c3fe75bd
Limiana <5073202+Limiana@users.noreply.github.com>
2023-02-08 15:57:58 +0300

    ImGui methods

--- ECommons/ImGuiMethods/ImGuiEx.cs
+++ ECommons/ImGuiMethods/ImGuiEx.cs
@@ -14,6 +14,24 @@ namespace ECommons.ImGuiMethods;
 
 public static class ImGuiEx
 {
+    public static bool HashSetCheckbox<T>(string label, T value, HashSet<T> collection)
+    {
+        var x = collection.Contains(value);
+        if(ImGui.Checkbox(label, ref x))
+        {
+            if (x)
+            {
+                collection.Add(value);
+            }
+            else
+            {
+                collection.Remove(value);
+            }
+            return true;
+        }
+        return false;
+    }
+
     public static Vector4 MutateColor(ImGuiCol col, byte r, byte g, byte b)
     {
         return ImGui.GetStyle().Colors[(int)col] with { X = (float)r / 255f, Y = (float)g / 255f, Z = (float)b / 255f };
