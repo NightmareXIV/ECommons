5ea4cc305d5e72b5f4e1c59a5e50685c059fb3db
Limiana <5073202+Limiana@users.noreply.github.com>
2023-04-02 18:35:51 +0300

    Update

--- ECommons/ImGuiMethods/GradientColor.cs
+++ ECommons/ImGuiMethods/GradientColor.cs
@@ -9,7 +9,7 @@ public static class GradientColor
     public static Vector4 Get(Vector4 start, Vector4 end, int Miliseconds = 1000)
     {
         var delta = (end - start) / (int)Miliseconds;
-        var time = Environment.TickCount % (Miliseconds * 2);
+        var time = Environment.TickCount64 % (Miliseconds * 2);
         if (time < Miliseconds)
         {
             return start + delta * (float)(time % Miliseconds);
