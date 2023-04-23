cffb64685c5701720b2b9231cb1c91c4e96ab7fc
Limiana <5073202+Limiana@users.noreply.github.com>
2023-04-23 21:51:12 +0300

    update

--- ECommons/StringHelpers/EqualStrings.cs
+++ ECommons/StringHelpers/EqualStrings.cs
@@ -15,7 +15,7 @@ namespace ECommons.StringHelpers
             Equalities.Add(values.ToHashSet());
         }
 
-        public static void RegisterEquality(params string[] values) => RegisterEquality(values);
+        public static void RegisterEquality(params string[] values) => RegisterEquality((IEnumerable<string>)values);
 
         public static bool Equals(string what, string with, StringComparison comparison = StringComparison.Ordinal)
         {
@@ -40,9 +40,9 @@ namespace ECommons.StringHelpers
             }
             return false;
         }
-        public static bool EqualsAny(string what, params string[] with) => EqualsAny(what, with);
+        public static bool EqualsAny(string what, params string[] with) => EqualsAny(what, (IEnumerable<string>)with);
         public static bool ESEqualsAny(this string what, IEnumerable<string> with) => EqualsAny(what, with);
-        public static bool ESEqualsAny(this string what, params string[] with) => EqualsAny(what, with);
+        public static bool ESEqualsAny(this string what, params string[] with) => EqualsAny(what, (IEnumerable<string>)with);
 
 
         public static bool EqualsAnyIgnoreCase(string what, IEnumerable<string> with)
@@ -53,9 +53,9 @@ namespace ECommons.StringHelpers
             }
             return false;
         }
-        public static bool EqualsAnyIgnoreCase(string what, params string[] with) => EqualsAnyIgnoreCase(what, with);
+        public static bool EqualsAnyIgnoreCase(string what, params string[] with) => EqualsAnyIgnoreCase(what, (IEnumerable<string>)with);
         public static bool ESEqualsAnyIgnoreCase(this string what, IEnumerable<string> with) => EqualsAnyIgnoreCase(what, with);
-        public static bool ESEqualsAnyIgnoreCase(this string what, params string[] with) => EqualsAnyIgnoreCase(what, with);
+        public static bool ESEqualsAnyIgnoreCase(this string what, params string[] with) => EqualsAnyIgnoreCase(what, (IEnumerable<string>)with);
 
         internal static void Dispose()
         {
