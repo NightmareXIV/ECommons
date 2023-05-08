fc8f810cdbe1f2116fef8f12ea3518c3e3c35b17
Taurenkey <github@taurenkey.co.uk>
2023-05-08 10:18:25 +0100

    Adds gamepad support

---
+++ ECommons/Gamepad/GamePad.cs
@@ -0,0 +1,32 @@
+﻿using Dalamud.Game.ClientState.GamePad;
+using ECommons.DalamudServices;
+using System.Collections.Generic;
+
+namespace ECommons.Gamepad
+{
+    public static class GamePad
+    {
+        public static Dictionary<GamepadButtons, string> ControllerButtons = new Dictionary<GamepadButtons, string>()
+        {
+            { GamepadButtons.None, "None" },
+            { GamepadButtons.DpadUp, "D-Pad Up"},
+            { GamepadButtons.DpadLeft, "D-Pad Left" },
+            { GamepadButtons.DpadDown, "D-Pad Down" },
+            { GamepadButtons.DpadRight, "D-Pad Right" },
+            { GamepadButtons.North, "△ / Y" },
+            { GamepadButtons.West, "□ / X" },
+            { GamepadButtons.South, "X / A" },
+            { GamepadButtons.East, "○ / B" },
+            { GamepadButtons.L1, "L1 / LB" },
+            { GamepadButtons.L2, "L2 / LT" },
+            { GamepadButtons.R1, "R1 / RB" },
+            { GamepadButtons.R2, "R2 / RT" },
+            { GamepadButtons.L3, "L3 / LS" },
+            { GamepadButtons.R3, "R3 / RS" },
+            { GamepadButtons.Start, "Options / Start" },
+            { GamepadButtons.Select, "Share / Back" }
+        };
+
+        public static bool IsButtonPressed(GamepadButtons button) => Svc.GamepadState.Pressed(button) == 1;
+    }
+}
