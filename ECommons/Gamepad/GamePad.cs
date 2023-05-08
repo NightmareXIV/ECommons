ff70dd486b8de7f459aec092268d2c80f3bbe5b4
Taurenkey <github@taurenkey.co.uk>
2023-05-08 10:36:50 +0100

    Adds documentation to gamepad

--- ECommons/Gamepad/GamePad.cs
+++ ECommons/Gamepad/GamePad.cs
@@ -6,6 +6,9 @@ namespace ECommons.Gamepad
 {
     public static class GamePad
     {
+        /// <summary>
+        /// Dictionary containing a mapping of <see cref="GamepadButtons"/> to Playstation / Xbox button names.
+        /// </summary>
         public static Dictionary<GamepadButtons, string> ControllerButtons = new Dictionary<GamepadButtons, string>()
         {
             { GamepadButtons.None, "None" },
@@ -26,9 +29,31 @@ namespace ECommons.Gamepad
             { GamepadButtons.Start, "Options / Start" },
             { GamepadButtons.Select, "Share / Back" }
         };
+        /// <summary>
+        /// Gets the "Enable gamepad" option from FFXIV to indicate if the gamepad has been enabled in FFXIV.
+        /// </summary>
+        /// <returns>FFXIV is using a controller.</returns>
         public static bool IsControllerEnabled() { Svc.GameConfig.TryGet(Dalamud.Game.Config.SystemConfigOption.PadAvailable, out bool enabled); return enabled; }
+
+        /// <summary>
+        /// Checks if a controller button has been pressed. Only true on the first frame it has been pressed.
+        /// </summary>
+        /// <param name="button">Button to check.</param>
+        /// <returns>Button has just been pressed.</returns>
         public static bool IsButtonPressed(GamepadButtons button) => Svc.GamepadState.Pressed(button) == 1;
+
+        /// <summary>
+        /// Checks if a controller button is currently held. Returns true for every frame it's held down.
+        /// </summary>
+        /// <param name="button">Button to check.</param>
+        /// <returns>Button is being held down.</returns>
         public static bool IsButtonHeld(GamepadButtons button) => Svc.GamepadState.Repeat(button) == 1;
+
+        /// <summary>
+        /// Checks if a controller button has just been released. Only true on the first frame after releasing.
+        /// </summary>
+        /// <param name="button">Button to check.</param>
+        /// <returns>Button has just been released.</returns>
         public static bool IsButtonJustReleased(GamepadButtons button) => Svc.GamepadState.Released(button) == 1;
 
     }
