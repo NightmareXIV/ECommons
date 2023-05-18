2c9eb76fe4597ed0c71222734ceca0890b04eaf1
Limiana <5073202+Limiana@users.noreply.github.com>
2023-05-18 20:05:56 +0300

    Add docs to Splatoon

--- ECommons/SplatoonAPI/Splatoon.cs
+++ ECommons/SplatoonAPI/Splatoon.cs
@@ -29,6 +29,10 @@ public static class Splatoon
         Svc.PluginInterface.GetIpcSubscriber<bool>("Splatoon.Unloaded").Subscribe(Reset);
     }
 
+    /// <summary>
+    /// Executed when connected or reconnected to Splatoon. Create and recreate your elements here. Once this event is fired, all already created elements become invalid.
+    /// </summary>
+    /// <param name="action">Action to be executed on connect</param>
     public static void SetOnConnect(Action action)
     {
         OnConnect = action;
@@ -77,26 +81,104 @@ public static class Splatoon
         }
     }
 
+    /// <returns>Whether currently connected to Splatoon</returns>
     public static bool IsConnected()
     {
         return Instance != null;
     }
 
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - system tick at which it should be destroyed, where current system tick can be obtained with Environment.TickCount64 parameter.</param>
+    /// <returns>Whether operation was successful</returns>
     public static bool AddDynamicElement(string name, Element e, long[] DestroyCondition)
     {
         return AddDynamicElements(name, new Element[] { e }, DestroyCondition);
     }
 
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - system tick at which it should be destroyed, where current system tick can be obtained with Environment.TickCount64 parameter.</param>
+    /// <returns>Whether operation was successful</returns>
     public static bool AddDynamicElement(string name, Element e, long DestroyCondition)
     {
         return AddDynamicElements(name, new Element[] { e }, new long[] { DestroyCondition });
     }
 
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - system tick at which it should be destroyed, where current system tick can be obtained with Environment.TickCount64 parameter.</param>
+    /// <returns>Whether operation was successful</returns>
     public static bool AddDynamicElements(string name, Element[] e, long DestroyCondition)
     {
         return AddDynamicElements(name, e, new long[] { DestroyCondition });
     }
 
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - amount of seconds before element will be destroyed from the moment of it's addition.</param>
+    /// <returns>Whether operation was successful</returns>
+    public static bool AddDynamicElement(string name, Element e, float[] DestroyCondition)
+    {
+        return AddDynamicElements(name, new Element[] { e }, DestroyCondition);
+    }
+
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - amount of seconds before element will be destroyed from the moment of it's addition.</param>
+    /// <returns>Whether operation was successful</returns>
+    public static bool AddDynamicElement(string name, Element e, float DestroyCondition)
+    {
+        return AddDynamicElements(name, new Element[] { e }, new float[] { DestroyCondition });
+    }
+
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - amount of seconds before element will be destroyed from the moment of it's addition.</param>
+    /// <returns>Whether operation was successful</returns>
+    public static bool AddDynamicElements(string name, Element[] e, float DestroyCondition)
+    {
+        return AddDynamicElements(name, e, new float[] { DestroyCondition });
+    }
+
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyConditionF">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - amount of seconds before element will be destroyed from the moment of it's addition.</param>
+    /// <returns>Whether operation was successful</returns>
+    public static bool AddDynamicElements(string name, Element[] e, float[] DestroyConditionF)
+    {
+        var dCond = DestroyConditionF.Select(x => x > 0 ? ((long)(x * 1000f) + Environment.TickCount64) : (long)x).ToArray();
+        return AddDynamicElements(name, e, dCond);
+    }
+
+    /// <summary>
+    /// Add persistent dynamic element.
+    /// </summary>
+    /// <param name="name">Non-unique namespace of the element</param>
+    /// <param name="e">Element or array of elements</param>
+    /// <param name="DestroyCondition">Destroy condition or array of them where: -2 is destroy on zone change; -1 is destroy on combat end; 0 is never destroy; any other number - system tick at which it should be destroyed, where current system tick can be obtained with Environment.TickCount64 parameter.</param>
+    /// <returns>Whether operation was successful</returns>
     public static bool AddDynamicElements(string name, Element[] e, long[] DestroyCondition)
     {
         if (!IsConnected())
@@ -131,6 +213,11 @@ public static class Splatoon
         }
     }
 
+    /// <summary>
+    /// Display certain element in next frame only. 
+    /// </summary>
+    /// <param name="e">Element to display</param>
+    /// <returns>Whether operation was successful</returns>
     public static bool DisplayOnce(Element e)
     {
         if (!IsConnected())
@@ -155,6 +242,11 @@ public static class Splatoon
         }
     }
 
+    /// <summary>
+    /// Removes dynamic elements with specific name. If more than one element was registered under certain name, all of these elements will be removed.
+    /// </summary>
+    /// <param name="name">Name of element(s)</param>
+    /// <returns>Whether operation was successful</returns>
     public static bool RemoveDynamicElements(string name)
     {
         if (!IsConnected())
@@ -174,6 +266,11 @@ public static class Splatoon
         }
     }
 
+    /// <summary>
+    /// Attempts to decode element that was encoded into JSON.
+    /// </summary>
+    /// <param name="input">Input string, you can export it from Splatoon. Make sure element is enabled!</param>
+    /// <returns>Element that is ready for use or null if failed</returns>
     public static Element DecodeElement(string input)
     {
         var method = Instance.GetType().Assembly.GetType("Splatoon.SplatoonScripting.ScriptingEngine", true).GetMethod("TryDecodeElement", BindingFlags.Public | BindingFlags.Static);
@@ -189,6 +286,11 @@ public static class Splatoon
         }
     }
 
+    /// <summary>
+    /// Work in progress on this function
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     [Obsolete("Work in progress")]
     public static object DecodeLayout(string input)
     {
