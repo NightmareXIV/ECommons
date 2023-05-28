22c24f5d1cae3db0361d296f4f68f54f56b0bd8c
秋水 <1123993881@qq.com>
2023-05-28 11:24:35 +0800

    feat: add a CmdManager to easy init cmd.

---
+++ ECommons/Commands/SubCmdAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ECommons.Commands;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class SubCmdAttribute : Attribute
+{
+    public string SubCommand { get; }
+    public string HelpMessage { get; }
+
+    public SubCmdAttribute(string subCommand, string helpMessage = "")
+    {
+        this.SubCommand = subCommand;
+        this.HelpMessage = helpMessage;
+    }
+}
