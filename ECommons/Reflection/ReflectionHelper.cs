59d30a3f96006958d094cb969246ba59314299e5
Limiana <5073202+Limiana@users.noreply.github.com>
2023-09-21 16:51:29 +0300

    update reflection

--- ECommons/Reflection/ReflectionHelper.cs
+++ ECommons/Reflection/ReflectionHelper.cs
@@ -69,8 +69,6 @@ public static class ReflectionHelper
         return (T)Call(obj, name, values);
     }
 
-    public static object Call<T>(this object obj, string name, params object[] values) => (T)Call(obj, name, values);
-
     public static object CallStatic(this object obj, string type, string name, params object[] values)
     {
         var info = obj.GetType().Assembly.GetType(type).GetMethod(name, AllFlags, values.Select(x => x.GetType()).ToArray());
