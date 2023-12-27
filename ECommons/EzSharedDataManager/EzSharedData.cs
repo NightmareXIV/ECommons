51a5ae315511c01f7f40a5e138be98abc42b2de7
Limiana <5073202+Limiana@users.noreply.github.com>
2023-12-27 22:22:30 +0300

    improve EzSharedData

--- ECommons/EzSharedDataManager/EzSharedData.cs
+++ ECommons/EzSharedDataManager/EzSharedData.cs
@@ -1,98 +1,110 @@
-﻿using ECommons.Logging;
-using ECommons.DalamudServices;
+﻿using ECommons.DalamudServices;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
-namespace ECommons.EzSharedDataManager
+namespace ECommons.EzSharedDataManager;
+
+/// <summary>
+/// EzSharedData class aims to resolve following problems with built-in SharedData service.
+/// - It will automatically relinquish data upon plugin disposal, unless specifically instructed otherwise
+/// - You can use TryGet continuously and there will ever be only one usage instance, you do not have to worry about holding reference yourself. Although, this is slower than creating and holding reference yourself so please obtain and hold reference to the data in performance-critical functions such as Framework Update.
+/// </summary>
+public static class EzSharedData
 {
-    public static class EzSharedData
+    internal static List<string> Keep = [];
+    internal static Dictionary<string, object> Cache = [];
+    /// <summary>
+    /// Attempts to get existing data or create new data.
+    /// </summary>
+    /// <typeparam name="T">Data type. Note that only reference type works. If you need to use value type, use array and put one element there. Only .NET, Dalamud and Dalamud's libraries types; no submodules, nuget packages and your own custom-defined types. If you need to share complex structure, please use tuples and Alias any type feature https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-12.0/using-alias-types</typeparam>
+    /// <param name="Name">Shared data name. Please prefix it with your plugin's name.</param>
+    /// <param name="Data">Obtained reference.</param>
+    /// <param name="Mode">Creation mode, if any is needed.</param>
+    /// <param name="DefaultValue">Default value when creating data that doesn't exists.</param>
+    /// <returns>Whether data could be obtained</returns>
+    public static bool TryGet<T>(string Name, [NotNullWhen(true)] out T Data, CreationMode Mode = CreationMode.ReadOnly, T DefaultValue = null) where T : class
     {
-        internal static List<string> Keep = [];
-        internal static Dictionary<string, object> Cache = [];
-        public static bool TryGet<T>(string Name, out T Data, CreationMode Mode = CreationMode.ReadOnly, T DefaultValue = null) where T : class
+        if (Cache.TryGetValue(Name, out var data))
         {
-            if (Cache.TryGetValue(Name, out var data))
-            {
-                Data = (T)data;
-                return true;
-            }
-            if (Mode == CreationMode.ReadOnly)
-            {
-                if (Svc.PluginInterface.TryGetData<T>(Name, out Data))
-                {
-                    Cache[Name] = Data;
-                    return true;
-                }
-            }
-            else if (Mode == CreationMode.CreateAndRelinquish)
-            {
-                Data = Svc.PluginInterface.GetOrCreateData<T>(Name, () => DefaultValue);
-                Cache[Name] = Data;
-                return true;
-            }
-            else if (Mode == CreationMode.CreateAndKeep)
-            {
-                Data = Svc.PluginInterface.GetOrCreateData<T>(Name, () => DefaultValue);
-                Keep.Add(Name);
-                Cache[Name] = Data;
-                return true;
-            }
-            Data = default;
-            return false;
+            Data = (T)data;
+            return true;
         }
-
-        /* This doesn't works!
-         * public static bool TryGetValueType<T>(string Name, out T Data, CreationMode Mode = CreationMode.ReadOnly, T DefaultValue = default) where T:struct
+        if (Mode == CreationMode.ReadOnly)
         {
-            if (Cache.TryGetValue(Name, out var data))
+            if (Svc.PluginInterface.TryGetData<T>(Name, out Data))
             {
-                Data = ((T[])data)[0];
-                return true;
-            }
-            if (Mode == CreationMode.ReadOnly)
-            {
-                if (Svc.PluginInterface.TryGetData<T[]>(Name, out var array))
-                {
-                    Cache[Name] = array;
-                    Data = array[0];
-                    return true;
-                }
-            }
-            else if (Mode == CreationMode.CreateAndRelinquish)
-            {
-                Data = Svc.PluginInterface.GetOrCreateData<T[]>(Name, () => ([DefaultValue]))[0];
                 Cache[Name] = Data;
                 return true;
             }
-            else if (Mode == CreationMode.CreateAndKeep)
-            {
-                Data = Svc.PluginInterface.GetOrCreateData<T[]>(Name, () => ([DefaultValue]))[0];
-                Keep.Add(Name);
-                Cache[Name] = Data;
-                return true;
-            }
-            Data = default;
-            return false;
-        }*/
-
-        internal static void Dispose()
+        }
+        else if (Mode == CreationMode.CreateAndRelinquish)
         {
-            foreach(var x in Cache)
+            Data = Svc.PluginInterface.GetOrCreateData<T>(Name, () => DefaultValue);
+            Cache[Name] = Data;
+            return true;
+        }
+        else if (Mode == CreationMode.CreateAndKeep)
+        {
+            Data = Svc.PluginInterface.GetOrCreateData<T>(Name, () => DefaultValue);
+            Keep.Add(Name);
+            Cache[Name] = Data;
+            return true;
+        }
+        Data = default;
+        return false;
+    }
+
+    public static T GetOrCreate<T>(string Name, T DefaultValue = null) where T : class => Get(Name, CreationMode.CreateAndRelinquish, DefaultValue);
+
+    public static T Get<T>(string Name, CreationMode Mode = CreationMode.ReadOnly, T DefaultValue = null) where T : class
+    {
+        if (Cache.TryGetValue(Name, out var data))
+        {
+            return (T)data;
+        }
+        if (Mode == CreationMode.ReadOnly)
+        {
+            if (Svc.PluginInterface.TryGetData<T>(Name, out var Data))
             {
-                try
+                Cache[Name] = Data;
+                return Data;
+            }
+            else
+            {
+                throw new SharedDataNotReadyException(Name);
+            }
+        }
+        else if (Mode == CreationMode.CreateAndRelinquish)
+        {
+            var Data = Svc.PluginInterface.GetOrCreateData<T>(Name, () => DefaultValue);
+            Cache[Name] = Data;
+            return Data;
+        }
+        else if (Mode == CreationMode.CreateAndKeep)
+        {
+            var Data = Svc.PluginInterface.GetOrCreateData<T>(Name, () => DefaultValue);
+            Keep.Add(Name);
+            Cache[Name] = Data;
+            return Data;
+        }
+        throw new ArgumentOutOfRangeException(nameof(Mode));
+    }
+
+    internal static void Dispose()
+    {
+        foreach(var x in Cache)
+        {
+            try
+            {
+                if (!Keep.Contains(x.Key))
                 {
-                    if (!Keep.Contains(x.Key))
-                    {
-                        Svc.PluginInterface.RelinquishData(x.Key);
-                    }
+                    Svc.PluginInterface.RelinquishData(x.Key);
                 }
-                catch(Exception e)
-                {
-                    e.Log();
-                }
+            }
+            catch(Exception e)
+            {
+                e.Log();
             }
         }
     }
---
+++ ECommons/EzSharedDataManager/SharedDataNotReadyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.EzSharedDataManager;
+internal class SharedDataNotReadyException : Exception
+{
+    internal SharedDataNotReadyException(string dataName) : base($"Shared data tag {dataName} is not ready yet")
+    {
+    }
+}
