260d3b72a9c31041aa18fbe90053d997555b8871
Limiana <5073202+Limiana@users.noreply.github.com>
2024-05-10 02:08:13 +0300

    fix docs

--- ECommons/Singletons/ISingleton.cs
+++ ECommons/Singletons/ISingleton.cs
@@ -7,7 +7,7 @@ using ECommons;
 
 namespace ECommons.Singletons;
 /// <summary>
-/// Types from your plugin's assembly that implement this interface will be automatically created as singletons upon calling <see cref="ECommonsMain.Init"/>.
+/// Types from your plugin's assembly that implement this interface will be automatically created as singletons upon calling <see cref="SingletonManager.Initialize"/>.
 /// </summary>
 public interface ISingleton
 {
