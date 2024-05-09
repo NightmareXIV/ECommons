b7fd904d3ade809e78535010fa13574818bdc7a2
Limiana <5073202+Limiana@users.noreply.github.com>
2024-05-09 23:15:58 +0300

    remove save notifications

--- ECommons/SimpleGui/ConfigWindow.cs
+++ ECommons/SimpleGui/ConfigWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Dalamud.Interface.Windowing;
+using ECommons.Logging;
 using ECommons.Configuration;
 using ECommons.DalamudServices;
 using ECommons.ImGuiMethods;
@@ -32,12 +33,12 @@ public class ConfigWindow : Window
         if(EzConfigGui.Config != null)
         {
             Svc.PluginInterface.SavePluginConfig(EzConfigGui.Config);
-            Notify.Success("Configuration saved");
+            PluginLog.Debug("Configuration saved");
         }
         if(EzConfig.Config != null)
         {
             EzConfig.Save();
-            Notify.Success("Configuration saved");
+						PluginLog.Debug("Configuration saved");
         }
         EzConfigGui.OnClose?.Invoke();
     }
