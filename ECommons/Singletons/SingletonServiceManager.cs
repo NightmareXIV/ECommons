fc9be016a02bda8b6547c61d718f595dc0f32e49
Limiana <5073202+Limiana@users.noreply.github.com>
2024-05-11 22:04:08 +0300

    change singleton service manager once again

--- ECommons/Singletons/SingletonServiceManager.cs
+++ ECommons/Singletons/SingletonServiceManager.cs
@@ -3,56 +3,54 @@ using ECommons.Reflection;
 using System;
 using System.Collections.Generic;
 using ECommons;
+using System.Reflection;
+using System.Linq;
 
 namespace ECommons.Singletons;
 /// <summary>
-/// Simple singleton service manager. Extend this class and fill it up with fields/properties of your singleton services, then create an instance of it whenever you want. Any uninitialized instances will be initialized upon constructor call, and anything that implements <see cref="IDisposable"/> will be disposed together with <see cref="ECommonsMain.Dispose"/> call.
+/// Simple singleton service manager. Create a static class and fill it up with fields/properties of your singleton services, then call <see cref="Initialize"/> with your static class type whenever you want. Any uninitialized instances will be initialized upon <see cref="Initialize"/> call, and anything that implements <see cref="IDisposable"/> will be disposed together with <see cref="ECommonsMain.Dispose"/> call.
 /// </summary>
-public abstract class SingletonServiceManager
+public static class SingletonServiceManager
 {
-		internal static List<SingletonServiceManager> RegisteredManagers = [];
+		internal static List<Type> Types = [];
 
 		internal static void DisposeAll()
 		{
-				foreach(var x in RegisteredManagers)
+				foreach(var x in Types)
 				{
-						x.Dispose();
+						foreach (var t in x.GetFieldPropertyUnions(ReflectionHelper.AllFlags).Reverse())
+						{
+								var value = t.GetValue(null);
+								if (value is IDisposable disposable)
+								{
+										try
+										{
+												PluginLog.Debug($"Disposing singleton instance of {t.UnionType.FullName}");
+												disposable.Dispose();
+										}
+										catch (Exception e)
+										{
+												e.Log();
+										}
+								}
+								t.SetValue(null, null);
+						}
 				}
-				RegisteredManagers = null!;
+				Types = null!;
 		}
 
-		public SingletonServiceManager()
+		public static void Initialize(Type staticType)
 		{
-				RegisteredManagers.Add(this);
-				foreach (var x in this.GetType().GetFieldPropertyUnions(ReflectionHelper.AllFlags))
+				Types.Add(staticType);
+				foreach (var x in staticType.GetFieldPropertyUnions(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static))
 				{
-						var value = x.GetValue(this);
+						var value = x.GetValue(null);
 						if(value == null)
 						{
 								try
 								{
 										PluginLog.Debug($"Creating singleton instance of {x.UnionType.FullName}");
-										x.SetValue(this, Activator.CreateInstance(x.UnionType, true));
-								}
-								catch(Exception e)
-								{
-										e.Log();
-								}
-						}
-				}
-		}
-
-		private void Dispose()
-		{
-				foreach (var x in this.GetType().GetFieldPropertyUnions(ReflectionHelper.AllFlags))
-				{
-						var value = x.GetValue(this);
-						if(value is IDisposable disposable)
-						{
-								try
-								{
-										PluginLog.Debug($"Disposing singleton instance of {x.UnionType.FullName}");
-										disposable.Dispose();
+										x.SetValue(null, Activator.CreateInstance(x.UnionType, true));
 								}
 								catch(Exception e)
 								{
