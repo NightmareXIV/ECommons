ba17217dcc4990b31956e2ba0eefd63577ea137f
Limiana <5073202+Limiana@users.noreply.github.com>
2024-06-30 01:27:58 +0300

    Update namespaces

--- ECommons/ExcelServices/Sheets/ExtendedBaseParam.cs
+++ ECommons/ExcelServices/Sheets/ExtendedBaseParam.cs
@@ -2,7 +2,7 @@
 using Lumina.Excel;
 using Lumina;
 
-namespace AutoRetainer.Sheets;
+namespace ECommons.ExcelServices.Sheets;
 
 //This code is authored by Caraxi https://github.com/Caraxi/SimpleTweaksPlugin/
 [Sheet("BaseParam")]
