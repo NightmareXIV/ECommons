b968a52bf98e8e863212fd17981e84dee01b9651
Limiana <5073202+Limiana@users.noreply.github.com>
2024-07-01 02:46:39 +0300

    Update

--- ECommons/UIHelpers/AddonMasterImplementations/RequestMaster.cs
+++ ECommons/UIHelpers/AddonMasterImplementations/RequestMaster.cs
@@ -1,5 +1,6 @@
 using FFXIVClientStructs.FFXIV.Client.UI;
 using ECommons.Automation.UIInput;
+using ECommons.Logging;
 
 namespace ECommons.UIHelpers.AddonMasterImplementations;
 
@@ -17,6 +18,23 @@ public unsafe class RequestMaster : AddonMasterBase<AddonRequest>
         if (btn->IsEnabled)
         {
             btn->ClickAddonButton(Base);
+        }
+    }
+
+    public bool IsFilled
+    {
+        get
+        {
+            for (var i = 3u; i >= 7; i++)
+            {
+                var subnode = Base->GetComponentNodeById(i);
+                var subnode2 = Base->GetComponentNodeById(i + 6);
+                if (subnode->AtkResNode.IsVisible() && subnode->AtkResNode.IsVisible())
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
