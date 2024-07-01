b8ceb49fbf22f844689d53ff3e3973df33f11e9f
Taurenkey <github@taurenkey.co.uk>
2024-07-01 14:18:42 +0100

    RecipeNote master

---
+++ ECommons/UIHelpers/AddonMasterImplementations/RecipeNote.cs
@@ -0,0 +1,28 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using ECommons.Automation.UIInput;
+
+namespace ECommons.UIHelpers.AddonMasterImplementations;
+
+public partial class AddonMaster
+{
+    public unsafe class RecipeNote : AddonMasterBase<AddonRecipeNote>
+    {
+        public RecipeNote(nint addon) : base(addon)
+        {
+        }
+
+        public void Material(uint index, bool hq)
+        {
+            var resNode = (AtkUnitBase*)Addon->GetNodeById(88);
+            var subRes = (AtkUnitBase*)resNode->GetNodeById(89 + index);
+            var btnRes = hq ? (AtkUnitBase*)subRes->GetNodeById(13) : (AtkUnitBase*)subRes->GetNodeById(10);
+            var btn = hq ? btnRes->GetButtonNodeById(14) : btnRes->GetButtonNodeById(11);
+
+            if (btn->IsEnabled)
+            {
+                btn->ClickAddonButton(Base);
+            }
+        }
+    }
+}
