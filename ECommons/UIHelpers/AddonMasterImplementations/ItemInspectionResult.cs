614912a6a6bc51070e72dcfbfa6d9a9b0dae9b29
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-07-02 13:38:21 +0200

    Update ItemInspectionResult.cs

--- ECommons/UIHelpers/AddonMasterImplementations/ItemInspectionResult.cs
+++ ECommons/UIHelpers/AddonMasterImplementations/ItemInspectionResult.cs
@@ -1,4 +1,5 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI;
+﻿using Dalamud.Game.Text.SeStringHandling;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace ECommons.UIHelpers.AddonMasterImplementations;
@@ -11,6 +12,11 @@ public partial class AddonMaster
         }
 
         public ItemInspectionResult(void* addon) : base(addon) { }
+
+        public AtkTextNode* NameNode => Base->GetTextNodeById(26);
+        public AtkTextNode* DescNode => Base->GetTextNodeById(35);
+        public SeString ItemName => NameNode->NodeText.ExtractText();
+        public SeString Description => DescNode->NodeText.ExtractText();
 
         public AtkComponentButton* NextButton => Base->GetButtonNodeById(73);
         public AtkComponentButton* CloseButton => Base->GetButtonNodeById(74);
