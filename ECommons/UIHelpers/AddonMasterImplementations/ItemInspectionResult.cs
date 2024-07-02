788108ea4938840f93401c9dbd6868ffd4849198
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-07-02 13:19:04 +0200

    iteminspectionresult

---
+++ ECommons/UIHelpers/AddonMasterImplementations/ItemInspectionResult.cs
@@ -0,0 +1,21 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace ECommons.UIHelpers.AddonMasterImplementations;
+public partial class AddonMaster
+{
+    public unsafe class ItemInspectionResult : AddonMasterBase<AddonItemInspectionResult>
+    {
+        public ItemInspectionResult(nint addon) : base(addon)
+        {
+        }
+
+        public ItemInspectionResult(void* addon) : base(addon) { }
+
+        public AtkComponentButton* NextButton => Base->GetButtonNodeById(73);
+        public AtkComponentButton* CloseButton => Base->GetButtonNodeById(74);
+
+        public void Next() => ClickButtonIfEnabled(NextButton);
+        public void Close() => ClickButtonIfEnabled(CloseButton);
+    }
+}
