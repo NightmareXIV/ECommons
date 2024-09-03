4c0f3e33e4dbda20a4470da3b7bb153f0ad12ace
Limiana <5073202+Limiana@users.noreply.github.com>
2024-09-03 06:09:59 +0300

    Popup window

---
+++ ECommons/SimpleGui/PopupWindow.cs
@@ -0,0 +1,51 @@
+﻿using Dalamud.Interface.Windowing;
+using ECommons.DalamudServices;
+using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.SimpleGui;
+/// <summary>
+/// Allows creation of single-use popup windows. Disposes itself upon being closed.
+/// </summary>
+public class PopupWindow : Window
+{
+    private Action drawAction;
+    private WindowSystem WindowSystem;
+    public PopupWindow(Action content) : base($"{Svc.PluginInterface.Manifest.Name} - Notification##{Guid.NewGuid()}", ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.NoCollapse, true)
+    {
+        Configure(content);
+    }
+
+    public PopupWindow(string title, Action content) : base($"{title}##{Guid.NewGuid()}", ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.NoCollapse, true)
+    {
+        Configure(content);
+    }
+
+    [MemberNotNull(nameof(drawAction), nameof(WindowSystem))]
+    private void Configure(Action content)
+    {
+        this.SetSizeConstraints(new(100), new(float.MaxValue));
+        this.drawAction = content;
+        this.RespectCloseHotkey = false;
+        this.AllowPinning = false;
+        this.WindowSystem = new();
+        WindowSystem.AddWindow(this);
+        this.IsOpen = true;
+        Svc.PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
+    }
+
+    public override void Draw()
+    {
+        drawAction?.Invoke();
+    }
+
+    public override void OnClose()
+    {
+        Svc.PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
+    }
+}
