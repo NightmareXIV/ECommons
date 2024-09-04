8c5b1091677a3d6353cea69c02d6de463b10fbae
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-09-04 13:35:00 +0200

    add/organise misc player attributes

--- ECommons/GameHelpers/Player.cs
+++ ECommons/GameHelpers/Player.cs
@@ -2,14 +2,16 @@
 using Dalamud.Game.ClientState.Statuses;
 using ECommons.DalamudServices;
 using ECommons.ExcelServices;
-using ECommons.GameFunctions;
 using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.MJI;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using Lumina.Excel.GeneratedSheets;
 using System;
 using System.Numerics;
-using System.Runtime.CompilerServices;
+using GrandCompany = ECommons.ExcelServices.GrandCompany;
 #nullable disable
 
 namespace ECommons.GameHelpers;
@@ -24,23 +26,37 @@ public static unsafe class Player
     public static StatusList Status => Svc.ClientState.LocalPlayer.StatusList;
     public static string Name => Svc.ClientState.LocalPlayer?.Name.ToString();
     public static string NameWithWorld => GetNameWithWorld(Svc.ClientState.LocalPlayer);
+    public static string GetNameWithWorld(this IPlayerCharacter pc) => pc == null ? null : (pc.Name.ToString() + "@" + pc.HomeWorld.GameData.Name);
+
     public static int Level => Svc.ClientState.LocalPlayer?.Level ?? 0;
+    public static bool IsLevelSynced => PlayerState.Instance()->IsLevelSynced == 1;
+    public static short SyncedLevel => PlayerState.Instance()->SyncedLevel;
+
     public static bool IsInHomeWorld => Svc.ClientState.LocalPlayer.HomeWorld.Id == Svc.ClientState.LocalPlayer.CurrentWorld.Id;
     public static bool IsInHomeDC => Svc.ClientState.LocalPlayer.CurrentWorld.GameData.DataCenter.Row == Svc.ClientState.LocalPlayer.HomeWorld.GameData.DataCenter.Row;
     public static string HomeWorld => Svc.ClientState.LocalPlayer?.HomeWorld.GameData.Name.ToString();
     public static string CurrentWorld => Svc.ClientState.LocalPlayer?.CurrentWorld.GameData.Name.ToString();
+    public static string HomeDataCenter => Svc.Data.GetExcelSheet<World>().GetRow(Svc.ClientState.LocalPlayer.HomeWorld.Id).DataCenter.Value.Name.ToString();
+    public static string CurrentDataCenter => Svc.Data.GetExcelSheet<World>().GetRow(Svc.ClientState.LocalPlayer.CurrentWorld.Id).DataCenter.Value.Name.ToString();
+
     public static Character* Character => (Character*)Svc.ClientState.LocalPlayer.Address;
     public static BattleChara* BattleChara => (BattleChara*)Svc.ClientState.LocalPlayer.Address;
     public static GameObject* GameObject => (GameObject*)Svc.ClientState.LocalPlayer.Address;
     [Obsolete("Please use GameObject")]
     public static GameObject* IGameObject => (GameObject*)Svc.ClientState.LocalPlayer.Address;
+
     public static uint Territory => Svc.ClientState.TerritoryType;
+    public static TerritoryIntendedUseEnum TerritoryIntendedUse => (TerritoryIntendedUseEnum)Svc.Data.GetExcelSheet<TerritoryType>().GetRow(Territory).TerritoryIntendedUse;
+    public static bool IsInDuty => GameMain.Instance()->CurrentContentFinderConditionId != 0;
+    public static bool IsOnIsland => MJIManager.Instance()->IsPlayerInSanctuary == 1;
+
     public static Job Job => GetJob(Svc.ClientState.LocalPlayer);
     public static GrandCompany GrandCompany => (GrandCompany)PlayerState.Instance()->GrandCompany;
-    public static string GetNameWithWorld(this IPlayerCharacter pc) => pc == null ? null : (pc.Name.ToString() + "@" + pc.HomeWorld.GameData.Name);
     public static Job GetJob(this IPlayerCharacter pc) => (Job)pc.ClassJob.Id;
+
     public static Vector3 Position => Object.Position;
     public static float Rotation => Object.Rotation;
     public static float AnimationLock => *(float*)((nint)ActionManager.Instance() + 8);
     public static bool IsAnimationLocked => AnimationLock > 0;
+    public static bool IsMoving => AgentMap.Instance()->IsPlayerMoving == 1;
 }
