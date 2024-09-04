ba45f3d57f1e4b521e769b9e6b93f7537db05978
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-09-04 13:35:30 +0200

    add repo list utils

--- ECommons/Reflection/DalamudReflector.cs
+++ ECommons/Reflection/DalamudReflector.cs
@@ -287,4 +287,43 @@ public static class DalamudReflector
         }
         return false;
     }
+
+    public static bool HasRepo(string repository)
+    {
+        var conf = GetService("Dalamud.Configuration.Internal.DalamudConfiguration");
+        var repolist = (IEnumerable<object>)conf.GetFoP("ThirdRepoList");
+        if (repolist != null)
+            foreach (var r in repolist)
+                if ((string)r.GetFoP("Url") == repository)
+                    return true;
+        return false;
+    }
+
+    public static void AddRepo(string repository, bool enabled)
+    {
+        var conf = GetService("Dalamud.Configuration.Internal.DalamudConfiguration");
+        var repolist = (IEnumerable<object>)conf.GetFoP("ThirdRepoList");
+        if (repolist != null)
+            foreach (var r in repolist)
+                if ((string)r.GetFoP("Url") == repository)
+                    return;
+        var instance = Activator.CreateInstance(Svc.PluginInterface.GetType().Assembly.GetType("Dalamud.Configuration.ThirdPartyRepoSettings")!);
+        instance.SetFoP("Url", repository);
+        instance.SetFoP("IsEnabled", enabled);
+        conf.GetFoP<IList<object>>("ThirdRepoList").Add(instance!);
+    }
+
+    public static void ReloadPluginMasters()
+    {
+        var mgr = GetService("Dalamud.Plugin.Internal.PluginManager");
+        var pluginReload = mgr?.GetType().GetMethod("SetPluginReposFromConfigAsync", BindingFlags.Instance | BindingFlags.Public);
+        pluginReload?.Invoke(mgr, [true]);
+    }
+
+    public static void SaveDalamudConfig()
+    {
+        var conf = GetService("Dalamud.Configuration.Internal.DalamudConfiguration");
+        var configSave = conf?.GetType().GetMethod("QueueSave", BindingFlags.Instance | BindingFlags.Public);
+        configSave?.Invoke(conf, null);
+    }
 }
