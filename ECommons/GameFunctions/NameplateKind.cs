e34bbb2ff84b0647f86e2d50be7d9ce4cd5d9adb
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-09-17 06:13:20 -0500

    Expand NameplateKind enum with new PC states and roles
    The NameplateKind enumeration has been updated to include new
    categories for different types of player characters (PCs) in
    various states. The additions are:
    - InDutyPCNotInParty (3)
    - OutOfDutyandPartyPC (22)
    - InDutyPCInPartyTank (27)
    - InDutyPCInPartyHealer (28)
    - InDutyPCInPartyDPS (29)

--- ECommons/GameFunctions/NameplateKind.cs
+++ ECommons/GameFunctions/NameplateKind.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 namespace ECommons.GameFunctions;
 public enum NameplateKind
 {
+    InDutyPCNotInParty = 3  ,
     HostileNotEngaged = 7,
     Dead = 8,
     HostileEngagedSelfUndamaged = 9,
     HostileEngagedOther = 10,
     HostileEngagedSelfDamaged = 11,
     FriendlyBattleNPC = 12,
+    OutOfDutyandPartyPC = 22,
+    InDutyPCInPartyTank = 27,
+    InDutyPCInPartyHealer = 28,
+    InDutyPCInPartyDPS = 29,
 }
