fd2416c51f0fe0264e00cc6b7aa5aa60e46917be
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-10-13 00:30:04 +0200

    add correct focus checker and gcarmy addons

---
+++ ECommons/UIHelpers/AddonMasterImplementations/GcArmyTraining.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace ECommons.UIHelpers.AddonMasterImplementations;
+public partial class AddonMaster
+{
+    public unsafe class GcArmyTraining : AddonMasterBase<AtkUnitBase>
+    {
+        public GcArmyTraining(nint addon) : base(addon) { }
+        public GcArmyTraining(void* addon) : base(addon) { }
+
+        public AtkComponentButton* CloseButton => Addon->GetButtonNodeById(39);
+
+        public void Close() => ClickButtonIfEnabled(CloseButton);
+    }
+}
