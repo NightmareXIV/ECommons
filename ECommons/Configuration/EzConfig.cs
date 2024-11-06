e01b55420e3586a61058b5a6a2235965531717d7
Limiana <5073202+Limiana@users.noreply.github.com>
2024-11-06 04:52:13 +0300

    Highly experimental stuff

--- ECommons/Configuration/EzConfig.cs
+++ ECommons/Configuration/EzConfig.cs
@@ -19,6 +19,7 @@ namespace ECommons.Configuration;
 public static class EzConfig
 {
     public static string? PluginConfigDirectoryOverride { get; set; } =  null;
+    public static bool UseExternalWriter = false;
     public static string GetPluginConfigDirectory()
     {
         if(PluginConfigDirectoryOverride == null) return Svc.PluginInterface.GetPluginConfigDirectory();
@@ -123,42 +124,49 @@ public static class EzConfig
     {
         WasCalled = true;
         serializationFactory ??= DefaultSerializationFactory;
-        var serialized = serializationFactory.Serialize(Configuration, prettyPrint);
-        void Write()
+        var serialized = serializationFactory.Serialize(Configuration, prettyPrint) ?? throw new NullReferenceException();
+        if(appendConfigDirectory) path = Path.Combine(EzConfig.GetPluginConfigDirectory(), path);
+        if(UseExternalWriter)
         {
-            try
-            {
-                lock(Configuration)
-                {
-                    if(appendConfigDirectory) path = Path.Combine(EzConfig.GetPluginConfigDirectory(), path);
-                    var antiCorruptionPath = $"{path}.new";
-                    if(File.Exists(antiCorruptionPath))
-                    {
-                        var saveTo = $"{antiCorruptionPath}.{DateTimeOffset.Now.ToUnixTimeMilliseconds()}";
-                        PluginLog.Warning($"Detected unsuccessfully saved file {antiCorruptionPath}: moving to {saveTo}");
-                        Notify.Warning("Detected unsuccessfully saved configuration file.");
-                        File.Move(antiCorruptionPath, saveTo);
-                        PluginLog.Warning($"Success. Please manually check {saveTo} file contents.");
-                    }
-                    //PluginLog.Verbose($"From caller {GenericHelpers.GetCallStackID(999)} engaging anti-corruption mechanism, writing file to {antiCorruptionPath}");
-                    File.WriteAllText(antiCorruptionPath, serialized, Encoding.UTF8);
-                    //PluginLog.Verbose($"Now moving {antiCorruptionPath} to {path}");
-                    File.Move(antiCorruptionPath, path, true);
-                    //PluginLog.Verbose($"Configuration successfully saved.");
-                }
-            }
-            catch(Exception e)
-            {
-                e.Log();
-            }
-        }
-        if(writeFileAsync)
-        {
-            Task.Run(Write);
+            ExternalWriter.PlaceWriteOrder(new(path, serialized));
         }
         else
         {
-            Write();
+            void Write()
+            {
+                try
+                {
+                    lock(Configuration)
+                    {
+                        var antiCorruptionPath = $"{path}.new";
+                        if(File.Exists(antiCorruptionPath))
+                        {
+                            var saveTo = $"{antiCorruptionPath}.{DateTimeOffset.Now.ToUnixTimeMilliseconds()}";
+                            PluginLog.Warning($"Detected unsuccessfully saved file {antiCorruptionPath}: moving to {saveTo}");
+                            Notify.Warning("Detected unsuccessfully saved configuration file.");
+                            File.Move(antiCorruptionPath, saveTo);
+                            PluginLog.Warning($"Success. Please manually check {saveTo} file contents.");
+                        }
+                        //PluginLog.Verbose($"From caller {GenericHelpers.GetCallStackID(999)} engaging anti-corruption mechanism, writing file to {antiCorruptionPath}");
+                        File.WriteAllText(antiCorruptionPath, serialized, Encoding.UTF8);
+                        //PluginLog.Verbose($"Now moving {antiCorruptionPath} to {path}");
+                        File.Move(antiCorruptionPath, path, true);
+                        //PluginLog.Verbose($"Configuration successfully saved.");
+                    }
+                }
+                catch(Exception e)
+                {
+                    e.Log();
+                }
+            }
+            if(writeFileAsync)
+            {
+                Task.Run(Write);
+            }
+            else
+            {
+                Write();
+            }
         }
     }
 
