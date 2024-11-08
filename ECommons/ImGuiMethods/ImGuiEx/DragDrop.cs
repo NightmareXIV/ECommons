4d08fb15200b3ebe5d65c1c064b1ef45dd475a0c
Limiana <5073202+Limiana@users.noreply.github.com>
2024-11-08 22:00:42 +0300

    DragDrop class

---
+++ ECommons/ImGuiMethods/ImGuiEx/DragDrop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.ImGuiMethods;
+public static unsafe partial class ImGuiEx
+{
+    
+}
