1c8832b85e0beb57fe4ce59bcb7580e5e22b947f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-12-17 11:24:49 +0100

    Create SatisfactionSupplyResult.cs

---
+++ ECommons/UIHelpers/AddonMasterImplementations/SatisfactionSupplyResult.cs
@@ -0,0 +1,20 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace ECommons.UIHelpers.AddonMasterImplementations;
+
+public partial class AddonMaster
+{
+    public unsafe class SatisfactionSupplyResult : AddonMasterBase<AtkUnitBase>
+    {
+        public SatisfactionSupplyResult(nint addon) : base(addon) { }
+        public SatisfactionSupplyResult(void* addon) : base(addon) { }
+        public override string AddonDescription { get; } = "Custom Deliveries Rank Up Window";
+
+        public uint CurrentLevelXp => Addon->AtkValues[10].UInt;
+        public uint TotalLevelXp => Addon->AtkValues[11].UInt;
+
+        public AtkComponentButton* AcceptButton => Addon->GetButtonNodeById(36);
+
+        public void Accept() => ClickButtonIfEnabled(AcceptButton);
+    }
+}
