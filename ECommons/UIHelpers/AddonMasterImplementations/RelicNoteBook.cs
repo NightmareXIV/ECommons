523f9d27859f71fae639cc429edcb36e98a237a8
pot0to <19513249+pot0to@users.noreply.github.com>
2025-02-23 03:08:27 -0800

    ARR trial of the braves book has 3 dungeons and 3 fates, not 2

--- ECommons/UIHelpers/AddonMasterImplementations/RelicNoteBook.cs
+++ ECommons/UIHelpers/AddonMasterImplementations/RelicNoteBook.cs
@@ -32,7 +32,7 @@ public partial class AddonMaster
         {
             get
             {
-                RelicNoteBookDungeon[] relicNoteBookDungeon = new RelicNoteBookDungeon[2];
+                RelicNoteBookDungeon[] relicNoteBookDungeon = new RelicNoteBookDungeon[3];
                 for (int i = 0; i < relicNoteBookDungeon.Length; i++)
                 {
                     relicNoteBookDungeon[i] = new RelicNoteBookDungeon(this, Addon, GetDungeonCheckBox(i), i);
@@ -45,7 +45,7 @@ public partial class AddonMaster
         {
             get
             {
-                RelicNoteBookFate[] relicNoteBookFate = new RelicNoteBookFate[2];
+                RelicNoteBookFate[] relicNoteBookFate = new RelicNoteBookFate[3];
                 for (int i = 0; i < relicNoteBookFate.Length; i++)
                 {
                     relicNoteBookFate[i] = new RelicNoteBookFate(this, Addon, GetFateCheckBox(i), i);
@@ -80,15 +80,6 @@ public partial class AddonMaster
                 this.addon = addon;
                 this.checkbox = checkbox;
                 this.index = index;
-            }
-
-            public int Count
-            {
-                get
-                {
-                    var match = ExtractNumber().Match(addon->GetTextNodeById(9)->NodeText.ExtractText());
-                    return match.Success ? int.Parse(match.Value) : 0;
-                }
             }
 
             public AtkComponentCheckBox* CheckBox => checkbox;
