1427e8d3511e993501af61fcd641f52aefed61fc
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2025-05-04 23:41:07 -0400

    Created base WKSToolCustomize
    got halfway through when someone pointed out to me that it's been implimented in a better way to get it server side but. Wanted to push what I had for now -> finish working on it later

---
+++ ECommons/UIHelpers/AddonMasterImplementations/WKSToolCustomize.cs
@@ -0,0 +1,55 @@
+﻿using Dalamud.Memory;
+using ECommons.Automation;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Collections.Generic;
+
+namespace ECommons.UIHelpers.AddonMasterImplementations;
+
+public partial class AddonMaster
+{
+    /// <summary>
+    /// Moon Recipe Notebook
+    /// Works similary to the normal recipe, except it's specialized to moon items you're doing the quest for
+    /// </summary>
+    public unsafe partial class WKSToolCustomize : AddonMasterBase<AtkUnitBase>
+    {
+        public WKSToolCustomize(nint addon) : base(addon) { }
+        public WKSToolCustomize(void* addon) : base(addon) { }
+
+        public ClassSelector[] ClassList
+        {
+            get
+            {
+                var ret = new List<ClassSelector>();
+                for (var i = 0; i < 11; i++)
+                {
+                    var level = Addon->AtkValues[22 + i].UInt;
+                    if (level == 0)
+                        continue;
+
+                    var ClassName = MemoryHelper.ReadSeStringNullTerminated((nint)Addon->AtkValues[11 + i].String.Value).GetText();
+                    var ClassList = new ClassSelector(this, i)
+                    {
+                        ClassName = ClassName,
+                        Level = level,
+                    };
+                    ret.Add(ClassList);
+                }
+                return [.. ret];
+            }
+        }
+
+        public class ClassSelector(WKSToolCustomize master, int index)
+        {
+            public required string ClassName;
+            public uint Level;
+
+            public void Select()
+            {
+                Callback.Fire(master.Base, true, 11, index);
+            }
+        }
+
+        public override string AddonDescription => "Cosmic Relic Tool Ui";
+    }
+}
