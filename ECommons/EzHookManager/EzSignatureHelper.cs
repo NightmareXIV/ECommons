ad39d4b0f9b9e4f981eaf130533c90f96480cfec
Limiana <5073202+Limiana@users.noreply.github.com>
2025-08-04 09:07:52 +0300

    Update MapEffect

--- ECommons/EzHookManager/EzSignatureHelper.cs
+++ ECommons/EzHookManager/EzSignatureHelper.cs
@@ -2,6 +2,7 @@
 using ECommons.Reflection;
 using System;
 using System.Reflection;
+using TerraFX.Interop.Windows;
 
 namespace ECommons.EzHookManager;
 public static class EzSignatureHelper
@@ -20,6 +21,35 @@ public static class EzSignatureHelper
                     var method = obj.GetType().GetMethod(detourName, ReflectionHelper.AllFlags);
                     var hook = Activator.CreateInstance(x.FieldType, attr.Signature, Delegate.CreateDelegate(x.FieldType.GenericTypeArguments[0], method!.IsStatic ? null : obj, method), attr.AutoEnable, attr.Offset);
                     x.SetValue(obj, hook);
+                }
+            }
+            catch(TargetInvocationException te)
+            {
+                te.Log();
+                te.InnerException.Log();
+            }
+            catch(Exception e)
+            {
+                PluginLog.Error($"An error during initialization of attribute {attr} on {x.Name}");
+                e.Log();
+            }
+        }
+    }
+
+    public static void Initialize(Type type)
+    {
+        foreach(var x in type.GetFields(ReflectionHelper.AllFlags))
+        {
+            var attr = x.GetCustomAttribute<EzHookAttribute>();
+            try
+            {
+                if(attr != null)
+                {
+                    var detourName = attr.Detour ?? (x.Name.EndsWith("Hook") ? x.Name[..^4] : x.Name) + "Detour";
+                    //PluginLog.Debug($"Detour: {detourName}");
+                    var method = type.GetMethod(detourName, ReflectionHelper.AllFlags);
+                    var hook = Activator.CreateInstance(x.FieldType, attr.Signature, Delegate.CreateDelegate(x.FieldType.GenericTypeArguments[0], null, method), attr.AutoEnable, attr.Offset);
+                    x.SetValue(null, hook);
                 }
             }
             catch(TargetInvocationException te)
