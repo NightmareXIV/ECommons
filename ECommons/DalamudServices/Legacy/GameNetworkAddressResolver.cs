c6a7a0e2a9903883ee4a366a19c407f69dedee39
Limiana <5073202+Limiana@users.noreply.github.com>
2025-08-06 00:46:27 +0300

    Restore GameNetwork

---
+++ ECommons/DalamudServices/Legacy/GameNetworkAddressResolver.cs
@@ -0,0 +1,21 @@
+﻿using Dalamud.Game;
+using System;
+
+namespace ECommons.DalamudServices.Legacy;
+
+/// <summary>
+/// The address resolver for the <see cref="GameNetwork"/> class.
+/// </summary>
+internal sealed class GameNetworkAddressResolver : BaseAddressResolver
+{
+    /// <summary>
+    /// Gets the address of the ProcessZonePacketUp method.
+    /// </summary>
+    public IntPtr ProcessZonePacketUp { get; private set; }
+
+    /// <inheritdoc/>
+    protected override void Setup64Bit(ISigScanner sig)
+    {
+        this.ProcessZonePacketUp = sig.ScanText("48 89 5C 24 ?? 48 89 74 24 ?? 4C 89 64 24 ?? 55 41 56 41 57 48 8B EC 48 83 EC 70"); // unnamed in cs
+    }
+}
