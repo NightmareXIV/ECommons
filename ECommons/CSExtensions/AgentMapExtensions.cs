f2553bcd04034d258db62439abdfe8021d04c65b
Limiana <5073202+Limiana@users.noreply.github.com>
2025-08-07 04:53:49 +0300

    Just use nuget for official repo

---
+++ ECommons/CSExtensions/AgentMapExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.CSExtensions;
+public unsafe static class AgentMapExtensions
+{
+    extension(AgentMap agent)
+    {
+        public bool IsFlagMarkerSet => agent.FlagMarkerCount > 0;
+        public FlagMapMarker FlagMapMarker => agent.FlagMapMarkers[0];
+    }
+}
