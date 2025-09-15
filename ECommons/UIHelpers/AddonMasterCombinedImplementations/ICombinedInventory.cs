6f241ea4362a69c29e7da921a17e1b1c4890ca99
Limiana <5073202+Limiana@users.noreply.github.com>
2025-09-15 08:49:59 +0300

    Update addonmaster

---
+++ ECommons/UIHelpers/AddonMasterCombinedImplementations/ICombinedInventory.cs
@@ -0,0 +1,38 @@
+﻿using ECommons.UIHelpers.AddonMasterImplementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.UIHelpers.AddonMasterCombinedImplementations;
+public static unsafe partial class AddonMasterCombined
+{
+    public interface ICombinedInventory 
+    {
+        public void OpenArmoryChest();
+
+        public static ICombinedInventory Get()
+        {
+            {
+                if(GenericHelpers.TryGetAddonMaster<AddonMaster.Inventory>("Inventory", out var m) && m.IsAddonReady)
+                {
+                    return m;
+                }
+            }
+            {
+                if(GenericHelpers.TryGetAddonMaster<AddonMaster.InventoryLarge>("InventoryLarge", out var m) && m.IsAddonReady)
+                {
+                    return m;
+                }
+            }
+            {
+                if(GenericHelpers.TryGetAddonMaster<AddonMaster.InventoryExpansion>("InventoryExpansion", out var m) && m.IsAddonReady)
+                {
+                    return m;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ECommons/UIHelpers/AddonMasterImplementations/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using ECommons.UIHelpers.AddonMasterCombinedImplementations;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons.UIHelpers.AddonMasterImplementations;
+public unsafe partial class AddonMaster
+{
+    public class Inventory : AddonMasterBase<AddonInventory>, AddonMasterCombined.ICombinedInventory
+    {
+        public Inventory(nint addon) : base(addon)
+        {
+        }
+
+        public Inventory(void* addon) : base(addon)
+        {
+        }
+
+        public override string AddonDescription { get; } = "Inventory window";
+
+        public void OpenArmoryChest()
+        {
+            this.ClickButtonIfEnabled(this.Addon->AtkUnitBase.GetComponentButtonById(17));
+        }
+    }
+}
