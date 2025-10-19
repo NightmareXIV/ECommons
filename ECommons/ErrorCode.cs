37ccfcb81882e0f0ad3875e24ad4b57b146419a5
Limiana <5073202+Limiana@users.noreply.github.com>
2025-10-20 00:59:55 +0300

    Add ErrorCodes

---
+++ ECommons/ErrorCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommons;
+
+/// <summary>
+/// An enum that describes common errors that happen primarily for IPC communication use. These should not contain specific plugin names.
+/// </summary>
+public enum ErrorCode
+{
+    [Obfuscation] Success,
+    [Obfuscation] Player_is_not_logged_in,
+    [Obfuscation] Invalid_world_specified
+}
