b295d2701ed4c63d97d0cba18a430acb72b6ce10
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-11-04 18:59:07 +0000

    added services
    added IConsole, INamePlateGui, ITextureReadbackProvider, ISeStringEvaluator
    
    rearranged them alphabetically so it's easier to tell what's missing in the future

--- ECommons/DalamudServices/Svc.cs
+++ ECommons/DalamudServices/Svc.cs
@@ -12,17 +12,47 @@ namespace ECommons.DalamudServices;
 
 public class Svc
 {
+    [PluginService] public static IAddonEventManager AddonEventManager { get; private set; }
+    [PluginService] public static IAddonLifecycle AddonLifecycle { get; private set; }
+    [PluginService] public static IAetheryteList AetheryteList { get; private set; }
     [PluginService] public static IDalamudPluginInterface PluginInterface { get; private set; }
     [PluginService] public static IBuddyList Buddies { get; private set; }
     [PluginService] public static IChatGui Chat { get; private set; }
     [PluginService] public static IClientState ClientState { get; private set; }
     [PluginService] public static ICommandManager Commands { get; private set; }
     [PluginService] public static ICondition Condition { get; private set; }
+    [PluginService] public static IConsole Console { get; private set; }
+    [PluginService] public static IContextMenu ContextMenu { get; private set; }
     [PluginService] public static IDataManager Data { get; private set; }
+    [PluginService] public static IDtrBar DtrBar { get; private set; }
+    [PluginService] public static IDutyState DutyState { get; private set; }
     [PluginService] public static IFateTable Fates { get; private set; }
     [PluginService] public static IFlyTextGui FlyText { get; private set; }
     [PluginService] public static IFramework Framework { get; private set; }
+    [PluginService] public static IGameConfig GameConfig { get; private set; }
     [PluginService] public static IGameGui GameGui { get; private set; }
+    [PluginService] public static IGameInteropProvider Hook { get; private set; }
+    [PluginService] public static IGameInventory GameInventory { get; private set; }
+    [PluginService] public static IGameLifecycle GameLifecycle { get; private set; }
+    [PluginService] public static IGamepadState GamepadState { get; private set; }
+    [PluginService] public static IJobGauges Gauges { get; private set; }
+    [PluginService] public static IKeyState KeyState { get; private set; }
+    [PluginService] public static IMarketBoard MarketBoard { get; private set; }
+    [PluginService] public static INamePlateGui NamePlates { get; private set; }
+    [PluginService] public static INotificationManager NotificationManager { get; private set; }
+    [PluginService] public static IObjectTable Objects { get; private set; }
+    [PluginService] public static IPartyFinderGui PfGui { get; private set; }
+    [PluginService] public static IPartyList Party { get; private set; }
+    [PluginService] public static IPluginLog Log { get; private set; }
+    [PluginService] public static ISeStringEvaluator SeStringEvaluator { get; private set; }
+    [PluginService] public static ISigScanner SigScanner { get; private set; }
+    [PluginService] public static ITargetManager Targets { get; private set; }
+    [PluginService] public static ITextureProvider Texture { get; private set; }
+    [PluginService] public static ITextureSubstitutionProvider TextureSubstitution { get; private set; }
+    [PluginService] public static ITextureReadbackProvider TextureReadback { get; private set; }
+    [PluginService] public static ITitleScreenMenu TitleScreenMenu { get; private set; }
+    [PluginService] public static IToastGui Toasts { get; private set; }
+
     public static Legacy.IGameNetwork GameNetwork
     {
         get
@@ -31,31 +61,6 @@ public class Svc
             return field;
         }
     }
-    [PluginService] public static IJobGauges Gauges { get; private set; }
-    [PluginService] public static IKeyState KeyState { get; private set; }
-    [PluginService] public static IObjectTable Objects { get; private set; }
-    [PluginService] public static IPartyFinderGui PfGui { get; private set; }
-    [PluginService] public static IPartyList Party { get; private set; }
-    [PluginService] public static ISigScanner SigScanner { get; private set; }
-    [PluginService] public static ITargetManager Targets { get; private set; }
-    [PluginService] public static IToastGui Toasts { get; private set; }
-    [PluginService] public static IGameConfig GameConfig { get; private set; }
-    [PluginService] public static IGameLifecycle GameLifecycle { get; private set; }
-    [PluginService] public static IGamepadState GamepadState { get; private set; }
-    [PluginService] public static IDtrBar DtrBar { get; private set; }
-    [PluginService] public static IDutyState DutyState { get; private set; }
-    [PluginService] public static IGameInteropProvider Hook { get; private set; }
-    [PluginService] public static ITextureProvider Texture { get; private set; }
-    [PluginService] public static IPluginLog Log { get; private set; }
-    [PluginService] public static IAddonLifecycle AddonLifecycle { get; private set; }
-    [PluginService] public static IAetheryteList AetheryteList { get; private set; }
-    [PluginService] public static IAddonEventManager AddonEventManager { get; private set; }
-    [PluginService] public static IGameInventory GameInventory { get; private set; }
-    [PluginService] public static ITextureSubstitutionProvider TextureSubstitution { get; private set; }
-    [PluginService] public static ITitleScreenMenu TitleScreenMenu { get; private set; }
-    [PluginService] public static INotificationManager NotificationManager { get; private set; }
-    [PluginService] public static IContextMenu ContextMenu { get; private set; }
-    [PluginService] public static IMarketBoard MarketBoard { get; private set; }
 
     internal static bool IsInitialized = false;
     public static void Init(IDalamudPluginInterface pi)
