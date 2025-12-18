c33b90d27c29596f4d44422288f80bcc8190fe41
Limiana <5073202+Limiana@users.noreply.github.com>
2025-12-18 22:40:24 +0300

    CS extensions

---
+++ ECommons/CSExtensions/AtkEventStateExtensions.cs
@@ -0,0 +1,31 @@
+﻿using ECommons.Reflection;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommons.CSExtensions;
+
+public unsafe static class AtkEventStateExtensions
+{
+    extension(ref AtkEventState thisRef)
+    {
+        public byte UnkFlags3
+        {
+            get
+            {
+                fixed(AtkEventState* ptr = &thisRef)
+                {
+                    return *(byte*)(((nint)ptr) + 3);
+                }
+            }
+            set
+            {
+                fixed(AtkEventState* ptr = &thisRef)
+                {
+                    *(byte*)(((nint)ptr) + 3) = value;
+                }
+            }
+        }
+    }
+}
---
+++ ECommons/CSExtensions/InputTimerModuleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommons.CSExtensions;
+
+public unsafe static class InputTimerModuleExtensions
+{
+    extension(ref InputTimerModule thisRef)
+    {
+        public float Unk1C
+        {
+            get
+            {
+                fixed(InputTimerModule* ptr = &thisRef)
+                {
+                    return *(float*)(((nint)ptr) + 28);
+                }
+            }
+            set
+            {
+                fixed(InputTimerModule* ptr = &thisRef)
+                {
+                    *(float*)(((nint)ptr) + 28) = value;
+                }
+            }
+        }
+    }
+}
