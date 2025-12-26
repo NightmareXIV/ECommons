d906ee9d2c17bd20d54242e1df9a8b8b2b5556d8
Ethan Henderson <ethan@zbee.codes>
2025-12-26 15:41:28 -0700

    docs(GameObjectCtor): Add documentation to all public methods

--- ECommons/Hooks/GameObjectCtor.cs
+++ ECommons/Hooks/GameObjectCtor.cs
@@ -1,46 +1,52 @@
+#nullable disable
+
+#region
+
 using Dalamud.Hooking;
 using ECommons.DalamudServices;
 using ECommons.Logging;
 using System;
 
-#nullable disable
+#endregion
 
 namespace ECommons.Hooks;
 
 public class GameObjectCtor
 {
-    public const string Sig = "48 8D 05 ?? ?? ?? ?? C7 81 ?? ?? ?? ?? ?? ?? ?? ?? 48 89 01 48 8B C1 C3";
-
-    private delegate nint GameObjectConstructorDelegate(nint a1);
-
     public delegate void GameObjectConstructorCallbackDelegate(nint objectAddress);
 
-    private static Hook<GameObjectConstructorDelegate> GameObjectConstructorHook = null;
+    public const string Sig =
+        "48 8D 05 ?? ?? ?? ?? C7 81 ?? ?? ?? ?? ?? ?? ?? ?? 48 89 01 48 8B C1 C3";
 
-    private static event GameObjectConstructorCallbackDelegate _gameObjectConstructorEvent;
+    private static Hook<GameObjectConstructorDelegate> GameObjectConstructorHook;
+
+    private static event GameObjectConstructorCallbackDelegate
+        _gameObjectConstructorEvent;
+
     /// <summary>
-    ///     Add a <see cref="GameObjectConstructorCallbackDelegate"/> subscriber
+    ///     Add a <see cref="GameObjectConstructorCallbackDelegate" /> subscriber
     ///     to this event be called when a Game Object is created.
     /// </summary>
-    public static event GameObjectConstructorCallbackDelegate GameObjectConstructorEvent
-    {
-        add
+    public static event GameObjectConstructorCallbackDelegate
+        GameObjectConstructorEvent
         {
-            Hook();
-            _gameObjectConstructorEvent += value;
+            add
+            {
+                Hook();
+                _gameObjectConstructorEvent += value;
+            }
+            remove => _gameObjectConstructorEvent -= value;
         }
-        remove => _gameObjectConstructorEvent -= value;
-    }
-    
+
     internal static nint GameObjectConstructorDetour(nint a1)
     {
         try
         {
             var @event = _gameObjectConstructorEvent;
-            if (@event != null)
+            if(@event != null)
             {
                 // Iterate individual subscribers so a failing subscriber doesn't stop the rest.
-                foreach (var subscriber in @event.GetInvocationList())
+                foreach(var subscriber in @event.GetInvocationList())
                 {
                     try
                     {
@@ -48,17 +54,18 @@ public class GameObjectCtor
                             (GameObjectConstructorCallbackDelegate)subscriber;
                         subscriberMethod(a1);
                     }
-                    catch (Exception e)
+                    catch(Exception e)
                     {
                         e.Log();
                     }
                 }
             }
         }
-        catch (Exception e)
+        catch(Exception e)
         {
             e.Log();
         }
+
         return GameObjectConstructorHook!.Original(a1);
     }
 
@@ -69,37 +76,52 @@ public class GameObjectCtor
 
         if(Svc.SigScanner.TryScanText(Sig, out var ptr))
         {
-            GameObjectConstructorHook = Svc.Hook.HookFromAddress<GameObjectConstructorDelegate>(ptr, GameObjectConstructorDetour);
+            GameObjectConstructorHook =
+                Svc.Hook.HookFromAddress<GameObjectConstructorDelegate>(ptr,
+                    GameObjectConstructorDetour);
             Enable();
-            PluginLog.Information("Requested Game Object ctor() hook and successfully initialized");
+            PluginLog.Information(
+                "Requested Game Object ctor() hook and successfully initialized");
         }
         else
         {
-            PluginLog.Error($"Could not find Game Object ctor() signature");
+            PluginLog.Error("Could not find Game Object ctor() signature");
         }
     }
 
+    /// <remarks>
+    ///     Already called when you subscribe a delegate to
+    ///     <see cref="GameObjectConstructorEvent" />.
+    /// </remarks>
     public static void Enable()
     {
         if(GameObjectConstructorHook?.IsEnabled == false)
             GameObjectConstructorHook?.Enable();
     }
 
+    /// <remarks>
+    ///     Already called in <see cref="Dispose()" />.
+    /// </remarks>
     public static void Disable()
     {
         if(GameObjectConstructorHook?.IsEnabled == true)
             GameObjectConstructorHook?.Disable();
     }
 
+    /// <remarks>
+    ///     Already called in <see cref="ECommons.ECommonsMain.Dispose()" />.
+    /// </remarks>
     public static void Dispose()
     {
         if(GameObjectConstructorHook == null)
             return;
 
-        PluginLog.Information($"Disposing Game Object ctor() Hook");
+        PluginLog.Information("Disposing Game Object ctor() Hook");
         Disable();
-        if (!GameObjectConstructorHook.IsDisposed)
+        if(!GameObjectConstructorHook.IsDisposed)
             GameObjectConstructorHook?.Dispose();
         GameObjectConstructorHook = null;
     }
+
+    private delegate nint GameObjectConstructorDelegate(nint a1);
 }
