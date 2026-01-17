531abc0a60549f46c064d65895963ca4cf1f5cd5
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-01-17 10:22:40 +0000

    add showcondition

--- ECommons/EzDTR/EzDtr.cs
+++ ECommons/EzDTR/EzDtr.cs
@@ -16,6 +16,7 @@ public class EzDtr : IDisposable
     internal static List<EzDtr> Registered = [];
     internal Func<SeString> Text;
     internal Action<DtrInteractionEvent>? OnClick;
+    internal Func<bool>? ShowCondition;
 
     /// <summary>
     /// Creates a new <see cref="EzDtr"/>
@@ -23,22 +24,24 @@ public class EzDtr : IDisposable
     /// <param name="text">Function that returns an <see cref="SeString"/> for the entry's text.</param>
     /// <param name="onClick">Action performed whenever the entry is clicked</param>
     /// <param name="title">Name of the Dtr entry. Defaults to the plugin name.</param>
-    public EzDtr(Func<SeString> text, Action? onClick = null, string? title = null)
+    public EzDtr(Func<SeString> text, Action? onClick = null, string? title = null, Func<bool> showCondition = null)
     {
         title ??= DalamudReflector.GetPluginName();
         Text = text;
         OnClick = onClick != null ? _ => onClick() : null;
         Entry ??= Svc.DtrBar.Get(title);
+        ShowCondition = showCondition;
         Svc.Framework.Update += OnUpdate;
         Registered.Add(this);
     }
 
-    public EzDtr(Func<SeString> text, Action<DtrInteractionEvent>? onClick = null, string? title = null)
+    public EzDtr(Func<SeString> text, Action<DtrInteractionEvent>? onClick = null, string? title = null, Func<bool> showCondition = null)
     {
         title ??= DalamudReflector.GetPluginName();
         Text = text;
         OnClick = onClick;
         Entry ??= Svc.DtrBar.Get(title);
+        ShowCondition = showCondition;
         Svc.Framework.Update += OnUpdate;
         Registered.Add(this);
     }
@@ -47,6 +50,7 @@ public class EzDtr : IDisposable
     {
         if(Entry.Shown)
         {
+            if(ShowCondition != null && !ShowCondition()) return;
             Entry.Text = Text();
             if(OnClick != null)
                 Entry.OnClick = OnClick;
