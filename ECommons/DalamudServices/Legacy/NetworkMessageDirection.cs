8cd997910596377ea7d116f04b8c5e440522c5c9
Limiana <5073202+Limiana@users.noreply.github.com>
2026-01-28 06:37:55 +0300

    7.41

---
+++ ECommons/DalamudServices/Legacy/NetworkMessageDirection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommons.DalamudServices.Legacy;
+
+public enum NetworkMessageDirection
+{
+    /// <summary>
+    /// A zone down message.
+    /// </summary>
+    ZoneDown,
+
+    /// <summary>
+    /// A zone up message.
+    /// </summary>
+    ZoneUp,
+}
