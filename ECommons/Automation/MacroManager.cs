05496836b27eea57565a044215710df7cd91579b
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-02-18 11:24:02 +0000

    obsolete macro/utf8string in favour of cs

--- ECommons/Automation/MacroManager.cs
+++ ECommons/Automation/MacroManager.cs
@@ -1,9 +1,10 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using Dalamud.Game.Text.SeStringHandling;
+using ECommons.DalamudServices;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using FFXIVClientStructs.FFXIV.Client.UI.Shell;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace ECommons.Automation;
 #nullable disable
@@ -22,30 +23,66 @@ public static unsafe class MacroManager
 
     public static void Execute(IEnumerable<string> commands)
     {
-        var macroPtr = IntPtr.Zero;
         GenericHelpers.Safe(delegate
         {
-            var count = (byte)Math.Max(Macro.numLines, commands.Count());
-            if(count > Macro.numLines)
-            {
-                throw new InvalidOperationException("Macro was more than 15 lines!");
-            }
-            if(commands.Any(x => x.Length > 180))
-            {
-                throw new InvalidOperationException("Macro contained lines more than 180 symbols!");
-            }
-            if(commands.Any(x => x.Contains("\n") || x.Contains("\r") || x.Contains("\0") || Chat.SanitiseText(x).Length != x.Length))
-            {
-                throw new InvalidOperationException("Macro contained invalid symbols!");
-            }
-            macroPtr = Marshal.AllocHGlobal(Macro.size);
-            using var macro = new Macro(macroPtr, string.Empty, commands.ToArray());
-            Marshal.StructureToPtr(macro, macroPtr, false);
-
-            RaptureShellModule.Instance()->ExecuteMacro((FFXIVClientStructs.FFXIV.Client.UI.Misc.RaptureMacroModule.Macro*)macroPtr);
+            if(FromLines(commands) is RaptureMacroModule.Macro macro)
+                RaptureShellModule.Instance()->ExecuteMacro(&macro);
         });
-
-        Marshal.FreeHGlobal(macroPtr);
     }
 
+    public static RaptureMacroModule.Macro? FromLines(IEnumerable<string> commands)
+        => FromLines(commands.Select(x => (SeString)x));
+
+    public static RaptureMacroModule.Macro? FromLines(IEnumerable<SeString> commands)
+    {
+        if(commands.Count() > 15)
+        {
+            throw new InvalidOperationException("Macro was more than 15 lines!");
+        }
+        if(commands.Any(x => x.Encode().Length > 180))
+        {
+            throw new InvalidOperationException("Macro contained lines more than 180 symbols!");
+        }
+        if(commands.Any(x => x.TextValue is var str && (str.Contains('\n') || str.Contains('\r') || str.Contains('\0') || Chat.SanitiseText(str).Length != str.Length)))
+        {
+            throw new InvalidOperationException("Macro contained invalid symbols!");
+        }
+
+        var macro = new RaptureMacroModule.Macro();
+        macro.Name.Ctor();
+        foreach(ref var line in macro.Lines)
+            line.Ctor();
+
+        try
+        {
+            foreach(var (line, index) in commands.WithIndex())
+            {
+                if(line.Payloads.Count == 0 || line.Encode().Length == 0)
+                {
+                    macro.Lines[index].Clear();
+                    continue;
+                }
+
+                var encoded = line.Encode();
+                if(encoded.Length == 0 || encoded.Any(c => c == 0))
+                {
+                    macro.Lines[index].Clear();
+                    continue;
+                }
+
+                fixed(byte* encodedPtr = encoded)
+                {
+                    macro.Lines[index].SetString(encodedPtr);
+                }
+            }
+
+            return macro;
+        }
+        catch(Exception e)
+        {
+            Svc.Log.Error(e, $"Failed to create macro from lines");
+        }
+
+        return null;
+    }
 }
