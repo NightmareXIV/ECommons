a9b041e6a849966ea2a401ca6094750ee2e6899d
Limiana <5073202+Limiana@users.noreply.github.com>
2026-05-03 00:40:19 +0300

    Update

---
+++ ECommons/DalamudServices/Legacy/IChatMessageExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Dalamud.Game.Chat;
+using Dalamud.Game.Text;
+using FFXIVClientStructs;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommons.DalamudServices.Legacy;
+
+public static class IChatMessageExtensions
+{
+    extension(IChatMessage value)
+    {
+        public XivChatType Type
+        {
+            get
+            {
+                var ret = new LogInfo()
+                {
+                    LogKind = (ushort)value.LogKind,
+                    SourceKind = (EntityRelationKind)value.SourceKind,
+                    TargetKind = (EntityRelationKind)value.TargetKind,
+                };
+                return (XivChatType)ret.Value;
+            }
+        }
+    }
+}
