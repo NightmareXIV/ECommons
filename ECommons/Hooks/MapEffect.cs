efa957f4eedc3ec0f8412e63761cca3d4cf5ccdc
nightmarexivplugins@gmail.com <5073202+Limiana@users.noreply.github.com>
2026-08-05 03:18:48 +0300

    Experimental change map effect to CS

--- ECommons/Hooks/MapEffect.cs
+++ ECommons/Hooks/MapEffect.cs
@@ -5,19 +5,18 @@ using ECommons.Logging;
 using FFXIVClientStructs.FFXIV.Client.Game.InstanceContent;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using System;
+using TerraFX.Interop.Windows;
 
 namespace ECommons.Hooks;
 #nullable disable
 
 public static unsafe class MapEffect
 {
-    public const string Sig = "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 8B FA 41 0F B7 E8";
     public const string ExsMultisig = "40 55 41 57 48 83 EC ?? 48 83 B9";
 
-    public delegate long ProcessMapEffect(long a1, uint a2, ushort a3, ushort a4);
-    internal static Hook<ProcessMapEffect> ProcessMapEffectHook = null;
+    internal static Hook<ContentDirector.Delegates.ApplyMapEffect> ProcessMapEffectHook = null;
     private static Action<long, uint, ushort, ushort> Callback = null;
-    private static ProcessMapEffect OriginalDelegate;
+    private static ContentDirector.Delegates.ApplyMapEffect OriginalDelegate;
 
     private static class Ex
     {
@@ -83,7 +82,7 @@ public static unsafe class MapEffect
         }
     }
 
-    public static ProcessMapEffect Delegate
+    public static ContentDirector.Delegates.ApplyMapEffect Delegate
     {
         get
         {
@@ -93,23 +92,23 @@ public static unsafe class MapEffect
             }
             else
             {
-                OriginalDelegate ??= EzDelegate.Get<ProcessMapEffect>(Sig);
+                OriginalDelegate ??= EzDelegate.Get<ContentDirector.Delegates.ApplyMapEffect>(ContentDirector.Addresses.ApplyMapEffect.Value);
                 return OriginalDelegate;
             }
         }
     }
 
-    internal static long ProcessMapEffectDetour(long a1, uint a2, ushort a3, ushort a4)
+    internal static void ProcessMapEffectDetour(ContentDirector* a1, uint a2, ushort a3, ushort a4)
     {
         try
         {
-            Callback(a1, a2, a3, a4);
+            Callback((long)a1, a2, a3, a4);
         }
         catch(Exception e)
         {
             e.Log();
         }
-        return ProcessMapEffectHook.Original(a1, a2, a3, a4);
+        ProcessMapEffectHook.Original(a1, a2, a3, a4);
     }
 
     public static void Init(Action<long, uint, ushort, ushort> fullParamsCallback)
@@ -118,18 +117,11 @@ public static unsafe class MapEffect
         {
             throw new Exception("MapEffect is already initialized!");
         }
-        if(Svc.SigScanner.TryScanText(Sig, out var ptr))
-        {
-            Callback = fullParamsCallback;
-            ProcessMapEffectHook = Svc.Hook.HookFromAddress<ProcessMapEffect>(ptr, ProcessMapEffectDetour);
-            Enable();
-            PluginLog.Information($"Requested MapEffect hook and successfully initialized");
-            Ex.Initialize();
-        }
-        else
-        {
-            PluginLog.Error($"Could not find MapEffect signature");
-        }
+        Callback = fullParamsCallback;
+        ProcessMapEffectHook = Svc.Hook.HookFromAddress<ContentDirector.Delegates.ApplyMapEffect>(ContentDirector.Addresses.ApplyMapEffect.Value, ProcessMapEffectDetour);
+        Enable();
+        PluginLog.Information($"Requested MapEffect hook and successfully initialized");
+        Ex.Initialize();
     }
 
     public static void Enable()
